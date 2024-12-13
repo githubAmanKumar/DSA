9e54c95bda7a83ef164345226d1ab1ab64d15dae
Aman Kumar <amankumarsre5378@gmail.com>
2024-12-13 17:22:59 +0530

    arrays

--- 8array/demo.cpp
+++ 8array/demo.cpp
@@ -4,41 +4,41 @@ int main(){
 
 //============ Pattern Matching ===========
 
-//  int nums[] = {3,77,-3,4,};
-//  int smallest = nums[0];
-//  int biggest = nums[0];
-//  int n=sizeof(nums)/sizeof(int);
-//  for(int i=1;i<n;i++){
-//     if(smallest > nums[i]){
-//         smallest=nums[i];
-//     }    
-//  }
-//  for(int i=1;i<n;i++){
-//     if(biggest < nums[i]){
-//         biggest=nums[i];
-//     }    
-//  }
-//  cout << "smallest = " << smallest << "\n";
-//  cout << "biggest = " << biggest;
+ int nums[] = {3,77,-3,4,};
+ int smallest = nums[0];
+ int biggest = nums[0];
+ int n=sizeof(nums)/sizeof(int);
+ for(int i=1;i<n;i++){
+    if(smallest > nums[i]){
+        smallest=nums[i];
+    }    
+ }
+ for(int i=1;i<n;i++){
+    if(biggest < nums[i]){
+        biggest=nums[i];
+    }    
+ }
+ cout << "smallest = " << smallest << "\n";
+ cout << "biggest = " << biggest;
 
-//============== Reverse array-(2 pointer approach) ===========
+// ============== Reverse array-(2 pointer approach) ===========
 
-//  int arr[] = {1,2,3,4,5};
-//  int n = sizeof(arr)/sizeof(int);
+ int arr[] = {1,2,3,4,5};
+ int m = sizeof(arr)/sizeof(int);
 
-//  int start = 0;
-//  int end = n-1;
+ int start = 0;
+ int end = m-1;
 
-//  for(int i=0;i<n;i++){
-//     while(start < end){
-//         swap(arr[start],arr[end]);
-//         start++;end--;
-//     };
-//  };
+ for(int i=0;i<m;i++){
+    while(start < end){
+        swap(arr[start],arr[end]);
+        start++;end--;
+    };
+ };
 
-//  for(int i=0;i<n;i++){
-//     cout << arr[i];
-//  }; 
+ for(int i=0;i<m;i++){
+    cout << arr[i];
+ }; 
 
 return 0;
 };
